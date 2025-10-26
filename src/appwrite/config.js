7c8b28ca8b94268bc2dc336618a25ebe4fbe99b4
Prashant Adhikari <prashantadhikari440@gmail.com>
2025-10-26 09:45:39 +0545

    feat: add Appwrite configuration and implement search count tracking functionality

---
+++ src/appwrite/config.js
@@ -0,0 +1,11 @@
+import { Client, TablesDB, Query, ID } from "appwrite";
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
+
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+const tablesDB = new TablesDB(client);
+export { tablesDB, PROJECT_ID, DATABASE_ID, TABLE_ID, Query, ID };
---
+++ src/utils/updateSearchCount.js
@@ -0,0 +1,47 @@
+import {
+  tablesDB,
+  ID,
+  DATABASE_ID,
+  TABLE_ID,
+  Query,
+} from "../appwrite/config.js";
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  try {
+    // using appwrite SDK to check if a record for the searchTerm exists in the database
+    const response = await tablesDB.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.equal("searchTerm", searchTerm)],
+    });
+
+    // If record exists, update the count
+    if (response.rows.length > 0) {
+      const item = response.rows[0];
+      await tablesDB.updateRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: item.$id,
+        data: {
+          count: item.count + 1,
+        },
+      });
+    }
+    // else create a new record
+    else {
+      await tablesDB.createRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          searchTerm: searchTerm,
+          count: 1,
+          movieId: movie.id,
+          posterUrl: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+        },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
